35e538cdcacd9962df0623795bc4345067102194
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2024-12-18 11:40:50 +0530

    control flow and funciton 2/2 and error handling

--- cmd/tutorial2/main.go
+++ cmd/tutorial2/main.go
@@ -1,24 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var myString string = "Hello World"
 	printMe(myString)
 
 	var numerator int = 11
-	var denominator int = 2
-	var result, remainder int = intDivision(numerator, denominator)
-	fmt.Println("Result = ", result)
-	fmt.Println("Remainder = ", remainder)
-	fmt.Printf("intDivision(%v,%v) gives Result = %v, and Remainder = %v\n", numerator, denominator, result, remainder)
+	var denominator int = 3
+	var result, remainder, err = intDivision(numerator, denominator) // error handling as well
+
+	// error handling and if else statements
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if remainder == 0 {
+		fmt.Println("Remainder = ", remainder)
+		fmt.Println("Result = ", result)
+	} else {
+		fmt.Printf("intDivision(%v,%v) gives Result = %v, and Remainder = %v\n", numerator, denominator, result, remainder)
+	}
+
+	// switch
+	switch {
+	case err != nil:
+		fmt.Println(err.Error())
+	case remainder == 0:
+		fmt.Println("Remainder = ", remainder)
+		fmt.Println("Result = ", result)
+	default:
+		fmt.Printf("intDivision(%v,%v) gives Result = %v, and Remainder = %v\n", numerator, denominator, result, remainder)
+	}
+
+	switch remainder {
+	case 0:
+		fmt.Println("Remainder 0")
+	case 1, 2:
+		fmt.Println("Remainder 1 or 2")
+	}
 }
 
 func printMe(myString string) {
 	fmt.Println(myString)
 }
-func intDivision(numerator int, denominator int) (int, int) {
+func intDivision(numerator int, denominator int) (int, int, error) { //error return declarartion
+	// customn error generation
+	var err error
+	if denominator == 0 {
+		err = errors.New("Cannot divide by 0")
+		return 0, 0, err
+	}
 	var result int = numerator / denominator
 	var remainder int = numerator % denominator
-	return result, remainder
+	return result, remainder, nil
 }
